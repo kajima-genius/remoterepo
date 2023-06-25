23e01b38c1af85c5ef4789fd77ddaa40c6d9c8a1
Alex <osikov.alex2001@gmail.com>
2023-06-25 13:15:46 +0300

    Add test function to Main.java

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
@@ -5,5 +6,12 @@ public class Main {
         System.out.println("addd dsf dsfdsf");
         int z = 2 * 50;
         System.out.println("sdsdsd");
+
+        var arr = new int[]{1, 2, 3, 4, 5};
+        System.out.println(test(arr));
+    }
+
+    public static int test(int[] arr) {
+        return Arrays.stream(arr).sum();
     }
 }
