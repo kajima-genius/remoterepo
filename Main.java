f3c87ee01963ccd9b4caa21bcea1cebd56b53fe3
Alex <osikov.alex2001@gmail.com>
2023-06-25 13:07:19 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,5 +2,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("sout");
+        System.out.println("addd dsf dsfdsf");
+        int z = 2 * 50;
+        System.out.println("sdsdsd");
     }
 }
